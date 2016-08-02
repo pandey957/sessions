2f19a80704c38d7f264f45e72d5bd384dedf95a8
Satyendra Raj Pandeya <pandey957@gmail.com>
2016-08-02 20:47:48 +0530

    modified city name

--- neogrowth/zomato/start.py
+++ neogrowth/zomato/start.py
@@ -22,10 +22,11 @@ def insert_data(base_url, city, page = 1):
 
 if __name__ == '__main__':
     base_url = 'https://www.zomato.com/pune/restaurants?credit-card=1&page='
-    outfile = open('zomato_pune.csv','wb')
+    outfile = open('zomato.csv','wb')
     wrtr = UnicodeWriter(outfile)
     wrtr.writerow(['City, Category','Name', 'Phone', 'Address'])
     cities = ['ahmedabad','bangalore','chennai','hyderabad','mumbai','pune','kolkata','ncr']
     for city in cities:
+        base_url = base_url.replace('pune',city)
         insert_data(base_url, city)
     outfile.close()
